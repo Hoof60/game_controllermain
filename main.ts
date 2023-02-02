089a759f5210a0d86e3af4d4590748abf25f7bfc
Thomas Henderson <48155717+Hoof60@users.noreply.github.com>
2023-02-02 16:01:01 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,73 @@
-let weaponPower = 6
-let energyPower = 6
-let thrustPower = 6
-
-let paused = 0
-drawbars()
-
-function drawbars(){
-    
-    led.plotBarGraph(0, 100)
-    led.unplot(2,4)
+function drawbars () {
+    basic.clearScreen()
     plotBar(energyPower, 0)
-    plotBar(thrustPower, 2)
-    plotBar(weaponPower, 4)
+plotBar(thrustPower, 2)
+plotBar(weaponPower, 4)
 }
-
+input.onButtonPressed(Button.AB, function () {
+    if (paused == 0) {
+        serial.writeLine("p:1")
+        paused = 1
+    } else {
+        serial.writeLine("p:0")
+        paused = 0
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString.charAt(0) == "t") {
+        splitstring = receivedString.split(":")
+        thrustValue = parseFloat(splitstring[1]) * thrustscale[thrustPower]
+        serial.writeLine("t:" + thrustValue)
+    } else {
+        serial.writeLine(receivedString)
+    }
+})
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    do{   
+        line = serial.readLine()
+        if (line.charAt(0) == 'a'){
+            let newalive = parseInt(line.split(":")[1])
+            if (newalive == 0 && alive == 1){
+                alive = 0 
+                radio.sendString("a:0")
+                basic.showIcon(IconNames.Skull)
+            }
+            if (newalive == 1 && alive == 0){
+                reset()
+                radio.sendString("a:1")
+                alive = 1
+            }
+        }
+       } while (line.charAt(0) != 'e')
+radio.sendString("" + (line))
+})
+function reset () {
+    energyPower = 6
+    weaponPower = 6
+    thrustPower = 6
+    serial.writeLine("er:" + energyscale[energyPower])
+    serial.writeLine("wr:" + weaponscale[weaponPower])
+    drawbars()
+}
+let LPressed = 0
+let BPressed = 0
+let APressed = 0
+let thrustValue = 0
+let splitstring: string[] = []
+let paused = 0
+let thrustPower = 0
+let energyPower = 0
+let weaponPower = 0
+let shieldActive = 0
+let line = ""
+weaponPower = 6
+energyPower = 6
+thrustPower = 6
+let alive = 1
+let weaponscale = [0,0.2,0.4,0.6,0.8,0.9,1,1.4,2,2.5, 3]
+let energyscale = [0,0.2,0.4,1,1.25,1.5,1.75,2,3,4, 5]
+let thrustscale = [0,0.2,0.4,0.6,0.8, 0.9,1,1.5,1.75,2,2.25]
+drawbars()
 function plotBar(power:any, column:any){
     let temp = power
     let ledRow = 4
@@ -29,51 +83,19 @@ function plotBar(power:any, column:any){
         ledRow--
     }
 }
-
-radio.onReceivedString(function (receivedString) {
-
-    if (receivedString.charAt(0) == 't'){
-        let splitstring = receivedString.split(":")
-        let thrustValue = parseFloat(splitstring[1]) * (thrustPower/10)
-
-        serial.writeLine("t:" + thrustValue)
-    } else {
-    serial.writeLine(receivedString)
-    }
-})
-
-input.onPinPressed(TouchPin.P2, function () {
-    if (paused == 0){
-        serial.writeLine("p:1")
-        paused = 1
-    } else {
-        serial.writeLine("p:0")
-        paused = 0
-    }
-})
-
-
-
-let BPressed = 0
-let APressed = 0
-let LPressed = 0
-let shieldActive = 0
 let LHoldTime = 50
 let LPressedTimer = LHoldTime
 radio.setGroup(24)
-
-
-
 basic.forever(function () {
     if (APressed == 0 && input.buttonIsPressed(Button.A)) {
         APressed = 1
-        if (energyPower < 10) {
+        if (energyPower < 10 && alive == 1) {
             energyPower += 2
-            thrustPower--
-            weaponPower--
+            thrustPower += -1
+            weaponPower += -1
             drawbars()
-            serial.writeLine("er:" + energyPower)
-            serial.writeLine("wr:" + weaponPower)
+            serial.writeLine("er:" + energyscale[energyPower])
+            serial.writeLine("wr:" + weaponscale[weaponPower])
         }
     } else {
         if (APressed == 1 && !(input.buttonIsPressed(Button.A))) {
@@ -82,54 +104,37 @@ basic.forever(function () {
     }
     if (BPressed == 0 && input.buttonIsPressed(Button.B)) {
         BPressed = 1
-        if (weaponPower < 10) {
+        if (weaponPower < 10 && alive == 1) {
             weaponPower += 2
-            thrustPower--
-            energyPower--
+            thrustPower += -1
+            energyPower += -1
             drawbars()
-            serial.writeLine("er:" + energyPower)
-            serial.writeLine("wr:" + weaponPower)
+            serial.writeLine("er:" + energyscale[energyPower])
+            serial.writeLine("wr:" + weaponscale[weaponPower])
         }
     } else {
         if (BPressed == 1 && !(input.buttonIsPressed(Button.B))) {
             BPressed = 0
         }
     }
-
-    if (LPressed == 1 && !input.logoIsPressed()){
+    if (LPressed == 1 && !(input.logoIsPressed())) {
         LPressed = 0
-        if (LPressedTimer > 0 && thrustPower < 10) {
+        if (LPressedTimer > 0 && thrustPower < 10 && alive == 1) {
             thrustPower += 2
-            energyPower--
-            weaponPower--
+            energyPower += -1
+            weaponPower += -1
             drawbars()
-            serial.writeLine("er:" + energyPower)
-            serial.writeLine("wr:" + weaponPower)
+            serial.writeLine("er:" + energyscale[energyPower])
+            serial.writeLine("wr:" + weaponscale[weaponPower])
         }
         LPressedTimer = LHoldTime
-
     }
-
-    if (input.logoIsPressed()){
+    if (input.logoIsPressed()) {
         LPressed = 1
-        LPressedTimer--
-        if (LPressedTimer == 0){
-            energyPower = 6
-            weaponPower = 6
-            thrustPower = 6
-            drawbars()
-
-            music.playTone(Note.C, music.beat(BeatFraction.Eighth))
+        LPressedTimer += -1
+        if (LPressedTimer == 0 && alive == 1) {
+            reset()
+            music.playTone(262, music.beat(BeatFraction.Eighth))
         }
     }
-
-})
-serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function() {
-
-    let line = ""
-    do{   
-        line = serial.readLine()
-    } while (line.charAt(0) != 'e')
-    radio.sendString(line)
-
 })
